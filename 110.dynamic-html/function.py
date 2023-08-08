02f3c8d62c23810cbd4cbfa4413d8bf9831e6264
LukeJYK <809698350@qq.com>
2023-08-08 19:01:19 +0000

    110 fix

--- 110.dynamic-html/function.py
+++ 110.dynamic-html/function.py
@@ -1,7 +1,7 @@
 from datetime import datetime                                                   
 from random import sample  
 from os import path
-from time import time                                                           
+import time
 import os
 
 from jinja2 import Template
@@ -9,8 +9,8 @@ from jinja2 import Template
 SCRIPT_DIR = path.abspath(path.join(path.dirname(__file__)))
 
 def handler(event,context):
-
     # start timing
+    start = time.time()
     name = event.get('username')
     size = event.get('random_len')
     cur_time = datetime.now()
@@ -18,5 +18,6 @@ def handler(event,context):
     template = Template( open(path.join(SCRIPT_DIR, 'templates', 'template.html'), 'r').read())
     html = template.render(username = name, cur_time = cur_time, random_numbers = random_numbers)
     # end timing
+    execution_time = time.time() - start
     # dump stats 
-    return {'result': html}
+    return {'result': html, 'execution_time': execution_time}
