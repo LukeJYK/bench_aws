75c6d32828a5d1c37f55ce3c2d7da3f8531eca2b
LukeJYK <809698350@qq.com>
2023-08-09 18:15:10 +0000

    case501 cold start fin

--- 501.graph-pagerank/function.py
+++ 501.graph-pagerank/function.py
@@ -1,8 +1,8 @@
 import datetime
 import igraph
-
+import time
 def handler(event, context):
-
+    start = time.time()
     size = event.get('size')
 
     graph_generating_begin = datetime.datetime.now()
@@ -15,11 +15,12 @@ def handler(event, context):
 
     graph_generating_time = (graph_generating_end - graph_generating_begin) / datetime.timedelta(microseconds=1)
     process_time = (process_end - process_begin) / datetime.timedelta(microseconds=1)
-
+    exe = time.time() - start
     return {
             'result': result[0],
             'measurement': {
                 'graph_generating_time': graph_generating_time,
-                'compute_time': process_time
+                'compute_time': process_time,
+                'execution_time': exe
             }
     }
